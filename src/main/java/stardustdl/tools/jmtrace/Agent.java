1df621f60f9b4db239804ee6a9f57f5e83952161
StardustDL <stardustdl@163.com>
2021-12-09 16:16:46 +0800

    initial commit

---
+++ src/main/java/stardustdl/tools/jmtrace/Agent.java
@@ -0,0 +1,10 @@
+package stardustdl.tools.jmtrace;
+
+public class Agent {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+        System.out.println("Hello World");
+    }
+}
